fcf295b00b9a8904d77cd79855f9a2632469c336
iamvk-aitian <vishalkrishan4542@gmail.com>
2019-01-04 00:00:08 +0530

    Database

--- db1.php
+++ db1.php
@@ -1,11 +1,11 @@
 <?php
     $servername = "localhost";
-    $username = "hospice";
-    $password = "mohit";
-    $db_name="hospice";
-    $tbl_name1="login";
-    $tbl_name2="request";
-    $tbl_name3="available";
+    $username = "username_of_user";
+    $password = "pssword_for_username";
+    $db_name="databasename_in_mysql";
+    $tbl_name1="Login_User";
+    $tbl_name2="Requested_Medicines";
+    $tbl_name3="Available_Medicines";
     $conn = mysqli_connect($servername, $username, $password);
     $db_selected=mysqli_select_db($conn, $db_name);
 ?>
